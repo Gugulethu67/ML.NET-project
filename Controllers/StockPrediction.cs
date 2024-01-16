96d4ddd21299c7e1f1dc982ad2db1f2c0bbec28a
Gugulethu Mbele <51972252+Gugulethu67@users.noreply.github.com>
2024-01-16 20:41:51 +0200

    Add files via upload

---
+++ Controllers/StockPrediction.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Team21.MLModel;
+
+namespace Team21.Controllers
+{
+    
+    public class StockPrediction : Controller
+    {
+        [HttpGet]
+        public IActionResult Predict()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Predict(ModelInput input)
+        {
+            var prediction = MLModel.Predict(input);
+            ViewBag.Result = prediction;
+            return View();
+        }
+    }
+}
